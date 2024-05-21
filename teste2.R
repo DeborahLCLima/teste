bb2cc4e8a3f1ce7fbdad9f90270bc604e3fa99e2
DeborahLCLima <deborahlclima@gmail.com>
2024-05-21 16:56:53 -0300

    testando

--- teste2.R
+++ teste2.R
@@ -4,7 +4,9 @@
 neededPackages = c("terra", "R.utils", "sf", "dplyr", "stringr")
 pkgTest = function(x) { if (x %in% rownames(installed.packages()) == FALSE) { install.packages(x, dependencies= TRUE) }; library(x, character.only = TRUE) }
 for (package in neededPackages) { pkgTest(package) }
-
+library(usethis)
 #tentando entender
 #isso é um teste
 #hello world
+
+usethis :: use_github()

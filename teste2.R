f3839e2b17a3ce3fc73198fe7a7f035ed65b919c
DeborahLCLima <deborahlclima@gmail.com>
2024-05-21 16:53:48 -0300

    teste

--- teste2.R
+++ teste2.R
@@ -4,3 +4,7 @@
 neededPackages = c("terra", "R.utils", "sf", "dplyr", "stringr")
 pkgTest = function(x) { if (x %in% rownames(installed.packages()) == FALSE) { install.packages(x, dependencies= TRUE) }; library(x, character.only = TRUE) }
 for (package in neededPackages) { pkgTest(package) }
+
+#tentando entender
+#isso é um teste
+#hello world
